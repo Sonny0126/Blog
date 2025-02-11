29e9edba4d07c8e82cbbda7e4d0eb57040b26292
SeungMin Son <sonsmmm@naver.com>
2025-02-11 16:58:42 +0900

    투두리스트 확인

--- index.js
+++ index.js
@@ -124,12 +124,31 @@ addBtn.addEventListener('click', () => { // + 버튼으로 추가
   }
 })
 
-function createTodo () { // 할 일 추가 기능
+const savedTodoList =JSON.parse(localStorage.getItem('saved-items'));
+
+if(savedTodoList){//로컬데이터가 존재하면 실행행
+  for(let i=0; i<savedTodoList.length; i++){
+    createTodo(savedTodoList[i]);
+  }
+}
+
+const todoInput = document.querySelector('#todoInput');
+
+function createTodo (storageData) { // 할 일 추가 기능
+  let todoContents =todoInput.value;
+    if(storageData){
+      todoContents=storageData.contents
+    }
+
+    if (storageData && storageData.complete === true) {
+      newLi.classList.add('complete')
+    }
+
 	const todoList = document.querySelector('#todoList');
 	const newLi = document.createElement('li'); // li 생성
     const newBtn = document.createElement('button'); // button 생성
 	const newSpan = document.createElement('span'); // span 생성
-	const todoInput = document.querySelector('#todoInput');
+
       
     newLi.appendChild(newBtn); // li안에 button 담기
 	newLi.appendChild(newSpan); // li안에 span 담기
@@ -142,21 +161,38 @@ function createTodo () { // 할 일 추가 기능
 
   newBtn.addEventListener('click', () => { // 체크박스 클릭시 완료 표시
 		newLi.classList.toggle('complete');
+
+    saveItemsFn();
     });
 
   newLi.addEventListener('dblclick', () => {
     //더블 클릭시 삭제
     newLi.remove();
+    saveItemsFn();
   })
+}
 
   //전체 삭제버튼
   function deleteAll(){
     document.querySelectorAll('#todoList li').forEach(li => li.remove());
+    saveItemsFn();
   }
 
+  //로컬에 데이터 저장하기
+  function saveItemsFn(){
+    const saveItems=[];
+    for(let i=0; i<todoList.children.length; i++){
+      const todoObj ={
+        contents: todoList.children[i].querySelector('span').textContent, //리스트 목록
+        complete: todoList.children[i].classList.contains('complete')//완료 표시된 리스트
+      };
+      saveItems.push(todoObj); //배열 추가
+    }
 
 
-
-}
-
-
+   if (saveItems.length === 0) { // 데이터가 없다면 값 삭제
+        localStorage.removeItem('saved-items')
+    }else{
+        localStorage.setItem('saved-items', JSON.stringify(saveItems));
+    }
+  }
