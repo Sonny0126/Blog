f854598b5fda7ced1c3f275129f24fc17afc06cb
SeungMin Son <sonsmmm@naver.com>
2025-02-13 02:41:33 +0900

    동영상 수정

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const body = document.body;
 
 window.onload = () => { //이미지가 완전히 로드된 이후에 내부에 배경을 깔고, 켈린더 표시
     body.style.backgroundImage = "url('image/Night.png')";
-    displayCalendar();//이 부분이 없으면 달력 안보임
 };
 
 switchButton.addEventListener('click', () => {//밤일 때 전구 불 On, 낮일 때 전구 불 Off
